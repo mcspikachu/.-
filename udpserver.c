2124d1c05ccfde08fa94d9377106be3d3706f48b
mcspikachu <udaipsugu583@gmail.com>
2024-08-27 16:29:43 +0530

    Update udpserver.c

--- udpserver.c
+++ udpserver.c
@@ -2,43 +2,47 @@
 #include<netinet/in.h>
 #include<sys/socket.h>
 #include<string.h>
+#include<arpa/inet.h>  // Required for inet_addr()
 
 #define MAX 80
 #define PORT 43454
 #define SA struct sockaddr
 
-void func(int sockfd) {
+int main() 
+{
     char buff[MAX];
-    int n, clen;
-    struct sockaddr_in cli;
+    int n, clen, sockfd;
+	struct sockaddr_in servaddr, cli;
+
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // Prepare server address
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");  // Bind to localhost (127.0.0.1)
+    servaddr.sin_port = htons(PORT);
+
+    // Bind socket to the server address
+    bind(sockfd, (SA *)&servaddr, sizeof(servaddr));
+
     clen = sizeof(cli);
     
+    // Start communication
     for(;;) {
         bzero(buff, MAX);
         recvfrom(sockfd, buff, sizeof(buff), 0, (SA *)&cli, &clen);
-        printf("From client: %sTo client: ", buff);
-
+        printf("From client: %s", buff);
         bzero(buff, MAX);
+		printf("\nTo client: ");
         n = 0;
         while ((buff[n++] = getchar()) != '\n');
         sendto(sockfd, buff, sizeof(buff), 0, (SA *)&cli, clen);
 
         if (strncmp("exit", buff, 4) == 0) break;
     }
-}
 
-int main() {
-    int sockfd;
-    struct sockaddr_in servaddr;
-
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(PORT);
-
-    bind(sockfd, (SA *)&servaddr, sizeof(servaddr));
-    func(sockfd);
+    // Close the socket
     close(sockfd);
+    return 0;
 }
